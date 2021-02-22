967d0e38e1b2428d3cdcdb203f0302463209c5b7
Bogdan Radu <bogdanradu@Bogdans-MacBook-Air.local>
2021-02-22 20:24:34 +0100

    added build sh

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
    and offsets, using the standard names,
    these defines tell the C library to do so. */
 #define _LARGEFILE64_SOURCE
-#define _FILE_OFFSET_BITS 64 
+#define _FILE_OFFSET_BITS 64
 
 #include <stdlib.h>
 #include <unistd.h>
@@ -42,41 +42,44 @@ int print_entry(const char *filepath, const struct stat *info,
     localtime_r(&(info->st_mtime), &mtime);
 
     printf("%04d-%02d-%02d %02d:%02d:%02d",
-           mtime.tm_year+1900, mtime.tm_mon+1, mtime.tm_mday,
+           mtime.tm_year + 1900, mtime.tm_mon + 1, mtime.tm_mday,
            mtime.tm_hour, mtime.tm_min, mtime.tm_sec);
 
     if (bytes >= 1099511627776.0)
         printf(" %9.3f TiB", bytes / 1099511627776.0);
-    else
-    if (bytes >= 1073741824.0)
+    else if (bytes >= 1073741824.0)
         printf(" %9.3f GiB", bytes / 1073741824.0);
-    else
-    if (bytes >= 1048576.0)
+    else if (bytes >= 1048576.0)
         printf(" %9.3f MiB", bytes / 1048576.0);
-    else
-    if (bytes >= 1024.0)
+    else if (bytes >= 1024.0)
         printf(" %9.3f KiB", bytes / 1024.0);
     else
         printf(" %9.0f B  ", bytes);
 
-    if (typeflag == FTW_SL) {
-        char   *target;
-        size_t  maxlen = 1023;
+    if (typeflag == FTW_SL)
+    {
+        char *target;
+        size_t maxlen = 1023;
         ssize_t len;
 
-        while (1) {
-
+        while (1)
+        {
             target = malloc(maxlen + 1);
+
             if (target == NULL)
                 return ENOMEM;
 
             len = readlink(filepath, target, maxlen);
-            if (len == (ssize_t)-1) {
+
+            if (len == (ssize_t)-1)
+            {
                 const int saved_errno = errno;
                 free(target);
                 return saved_errno;
             }
-            if (len >= (ssize_t)maxlen) {
+
+            if (len >= (ssize_t)maxlen)
+            {
                 free(target);
                 maxlen += 1024;
                 continue;
@@ -88,25 +91,20 @@ int print_entry(const char *filepath, const struct stat *info,
 
         printf(" %s -> %s\n", filepath, target);
         free(target);
-
-    } else
-    if (typeflag == FTW_SLN)
+    }
+    else if (typeflag == FTW_SLN)
         printf(" %s (dangling symlink)\n", filepath);
-    else
-    if (typeflag == FTW_F)
+    else if (typeflag == FTW_F)
         printf(" %s\n", filepath);
-    else
-    if (typeflag == FTW_D || typeflag == FTW_DP)
+    else if (typeflag == FTW_D || typeflag == FTW_DP)
         printf(" %s/\n", filepath);
-    else
-    if (typeflag == FTW_DNR)
+    else if (typeflag == FTW_DNR)
         printf(" %s/ (unreadable)\n", filepath);
     else
         printf(" %s (unknown)\n", filepath);
 
     return 0;
 }
-
 
 int print_directory_tree(const char *const dirpath)
 {
@@ -127,22 +125,24 @@ int main(int argc, char *argv[])
 {
     int arg;
 
-    if (argc < 2) {
-
-        if (print_directory_tree(".")) {
+    if (argc < 2)
+    {
+        if (print_directory_tree("."))
+        {
             fprintf(stderr, "%s.\n", strerror(errno));
             return EXIT_FAILURE;
         }
-
-    } else {
-
-        for (arg = 1; arg < argc; arg++) {
-            if (print_directory_tree(argv[arg])) {
+    }
+    else
+    {
+        for (arg = 1; arg < argc; arg++)
+        {
+            if (print_directory_tree(argv[arg]))
+            {
                 fprintf(stderr, "%s.\n", strerror(errno));
                 return EXIT_FAILURE;
             }
         }
-
     }
 
     return EXIT_SUCCESS;
