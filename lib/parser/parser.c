588622675b47015fe31c7d3e72eb1ab311f89a35
Bogdan Radu <bogdanradu@Bogdans-MacBook-Air.local>
2021-04-05 23:13:46 +0200

    fixed verbose display error

--- lib/parser/parser.c
+++ lib/parser/parser.c
@@ -41,40 +41,47 @@ int handle_symbolic_link(const char *filepath)
     return 0;
 }
 
+void print_verbose_info(const struct stat *info)
+{
+    const double bytes = (double)info->st_size;
+    struct tm mtime;
+
+    localtime_r(&(info->st_mtime), &mtime);
+
+    printf("%04d-%02d-%02d %02d:%02d:%02d",
+           mtime.tm_year + 1900,
+           mtime.tm_mon + 1,
+           mtime.tm_mday,
+           mtime.tm_hour,
+           mtime.tm_min,
+           mtime.tm_sec);
+
+    if (bytes >= 1099511627776.0)
+        printf(" %9.3f TiB ", bytes / 1099511627776.0);
+    else if (bytes >= 1073741824.0)
+        printf(" %9.3f GiB ", bytes / 1073741824.0);
+    else if (bytes >= 1048576.0)
+        printf(" %9.3f MiB ", bytes / 1048576.0);
+    else if (bytes >= 1024.0)
+        printf(" %9.3f KiB ", bytes / 1024.0);
+    else
+        printf(" %9.0f B   ", bytes);
+}
+
 int print_entry(const char *filepath, const struct stat *info, const int typeflag, struct FTW *pathinfo)
 {
-    if (opt.verbose)
-    {
-        const double bytes = (double)info->st_size;
-        struct tm mtime;
-
-        localtime_r(&(info->st_mtime), &mtime);
-
-        printf("%04d-%02d-%02d %02d:%02d:%02d",
-               mtime.tm_year + 1900,
-               mtime.tm_mon + 1,
-               mtime.tm_mday,
-               mtime.tm_hour,
-               mtime.tm_min,
-               mtime.tm_sec);
-
-        if (bytes >= 1099511627776.0)
-            printf(" %9.3f TiB ", bytes / 1099511627776.0);
-        else if (bytes >= 1073741824.0)
-            printf(" %9.3f GiB ", bytes / 1073741824.0);
-        else if (bytes >= 1048576.0)
-            printf(" %9.3f MiB ", bytes / 1048576.0);
-        else if (bytes >= 1024.0)
-            printf(" %9.3f KiB ", bytes / 1024.0);
-        else
-            printf(" %9.0f B   ", bytes);
-    }
-
     switch (typeflag)
     {
     case FTW_SL:
         if (opt.include_links)
         {
+            if (opt.verbose)
+            {
+                print_verbose_info(info);
+            }
+
+            printf("%s (symlink)\n", filepath);
+
             int sym = 0;
             sym = handle_symbolic_link(filepath);
 
@@ -87,13 +94,19 @@ int print_entry(const char *filepath, const struct stat *info, const int typefla
         if (opt.include_links)
         {
             if (opt.verbose)
+            {
+                print_verbose_info(info);
                 printf("%s (dangling symlink)\n", filepath);
+            }
             else
+            {
                 printf("%s\n", filepath);
+            }
         }
         break;
 
     case FTW_F:
+        print_verbose_info(info);
         printf("%s\n", filepath);
         break;
 
@@ -102,9 +115,14 @@ int print_entry(const char *filepath, const struct stat *info, const int typefla
         if (opt.include_directories)
         {
             if (opt.verbose)
+            {
+                print_verbose_info(info);
                 printf("%s/ (directory)\n", filepath);
+            }
             else
+            {
                 printf("%s\n", filepath);
+            }
         }
         break;
 
@@ -112,9 +130,14 @@ int print_entry(const char *filepath, const struct stat *info, const int typefla
         if (opt.include_unreadable)
         {
             if (opt.verbose)
+            {
+                print_verbose_info(info);
                 printf("%s/ (unreadable)\n", filepath);
+            }
             else
+            {
                 printf("%s\n", filepath);
+            }
         }
         break;
 
@@ -123,9 +146,14 @@ int print_entry(const char *filepath, const struct stat *info, const int typefla
         if (opt.include_unknown)
         {
             if (opt.verbose)
+            {
+                print_verbose_info(info);
                 printf("%s (unknown)\n", filepath);
+            }
             else
+            {
                 printf("%s\n", filepath);
+            }
         }
         break;
     }
@@ -143,7 +171,7 @@ int print_directory_tree(struct OPTIONS *options)
         return errno = EINVAL;
     }
 
-    result = nftw(opt.root_file_path, print_entry, USE_FDS, FTW_PHYS);
+    result = nftw(opt.root_file_path, print_entry, USE_FDS, FTW_DEPTH | FTW_PHYS);
     if (result >= 0)
     {
         errno = result;
